f666515c27945d9698e9174ce2defc3c5a98413a
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2023-05-01 12:40:34 +0300

    feat: add keyboard to layout

--- src/js/container.js
+++ src/js/container.js
@@ -3,7 +3,7 @@ const rendering = () => `
   <h1>Виртуальная клавиатура</h1>
   <button class="btn">Shift+Ctrl</button>
   <label for="text">Поле для ввода текста:</label>
-  <textarea id="text" name="text" rows="25" cols="100"></textarea>
+  <textarea id="text" name="text"></textarea>
   <div class="keyboard"></div>
   </div>
   `;
--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
 import rendering from './js/container.js';
+import { en, ru } from './js/array.js';
 
 document.body.insertAdjacentHTML('beforeend', rendering());
+
+function keyboard() {
+  let keyboard = '';
+  for (let i = 0; i < en.length; i++) {
+    const item = en[i].length > 1 ? `<div class="key big-key ${en[i]}">${en[i]}</div>` : `<div class="key">${en[i]}</div>`;
+    keyboard += item;
+  }
+  document.querySelector('.keyboard').innerHTML = keyboard;
+}
+
+keyboard();
