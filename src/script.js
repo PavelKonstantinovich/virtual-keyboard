feb8a2c0575e8e28bdf69f913adeb5c91f1831ee
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2023-05-01 14:13:51 +0300

    feat: add class active to layout

--- src/script.js
+++ src/script.js
@@ -1,15 +1,25 @@
 import rendering from './js/container.js';
-import { en, ru } from './js/array.js';
+import { en, ru, which } from './js/array.js';
 
 document.body.insertAdjacentHTML('beforeend', rendering());
 
 function keyboard() {
   let keyboard = '';
   for (let i = 0; i < en.length; i++) {
-    const item = en[i].length > 1 ? `<div class="key big-key ${en[i]}">${en[i]}</div>` : `<div class="key">${en[i]}</div>`;
+    const item = en[i].length > 1 ? `<div id="${which[i]}" class="key big-key ${en[i]}">${en[i]}</div>` : `<div  id="${which[i]}" class="key">${en[i]}</div>`;
     keyboard += item;
   }
   document.querySelector('.keyboard').innerHTML = keyboard;
 }
 
 keyboard();
+
+console.log(ru.length);
+
+const arr = [];
+document.addEventListener('keydown', (e) => {
+  arr.push(e.which);
+  console.log(e);
+  document.querySelectorAll('.key').forEach((element) => element.classList.remove('active'));
+  document.getElementById(`${e.which}`).classList.add('active');
+});
